5d404cd26c0e88f3938d029ce52bcd486b223005
Daniel Sanchez <dansando@protonmail.com>
2022-01-17 19:21:22 -0500

    Renaming wireworld_step to wireworld_step_immutable. Adding wireworld_step_mutable. Using NumericArray instead of PackedArrays: this overall produces less performant code, but it allows the code to use exact numeric types, e.g. uint16_t and uint8_t.

--- libWireworld/libWireworld.c
+++ libWireworld/libWireworld.c
@@ -1,5 +1,7 @@
 #include "wireworld.c"
 
+#include "WolframNumericArrayLibrary.h"
+
 #include "WolframLibrary.h"
 
 EXTERN_C DLLEXPORT mint WolframLibrary_getVersion(void)
@@ -14,7 +16,7 @@ EXTERN_C DLLEXPORT int WolframLibrary_initialize(WolframLibraryData libData)
 
 /*****************************************************************************/
 
-EXTERN_C DLLEXPORT int wireworld_step(WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
+EXTERN_C DLLEXPORT int wireworld_step_immutable(WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
 {
 	mint error = LIBRARY_NO_ERROR;
 
@@ -22,27 +24,53 @@ EXTERN_C DLLEXPORT int wireworld_step(WolframLibraryData libData, mint argc, MAr
 		return LIBRARY_FUNCTION_ERROR;
 	}
 
-	MTensor state_tensor_in = MArgument_getMTensor(args[0]);
-	if (libData->MTensor_getRank(state_tensor_in) != 2) {
+	WolframNumericArrayLibrary_Functions naFuns = libData->numericarrayLibraryFunctions;
+	MNumericArray state_array_in = MArgument_getMNumericArray(args[0]);
+	if (naFuns->MNumericArray_getRank(state_array_in) != 2) {
 		return LIBRARY_RANK_ERROR;
 	}
-	if (libData->MTensor_getType(state_tensor_in) != MType_Integer) {
+	if (naFuns->MNumericArray_getType(state_array_in) != MNumericArray_Type_UBit16) {
 		return LIBRARY_TYPE_ERROR;
 	}
 
-	const mint *dims = libData->MTensor_getDimensions(state_tensor_in);
+	const mint *dims = naFuns->MNumericArray_getDimensions(state_array_in);
 
-	MTensor state_tensor_out;
-	error = libData->MTensor_new(MType_Integer, 2, dims, &state_tensor_out);
+	MNumericArray state_array_out;
+	error = naFuns->MNumericArray_new(MNumericArray_Type_UBit16, 2, dims, &state_array_out);
 	if (error) {
 		return error;
 	}
 
-	mint *state_in = libData->MTensor_getIntegerData(state_tensor_in);
-	mint *state_out = libData->MTensor_getIntegerData(state_tensor_out);
+	uint16_t *state_in = naFuns->MNumericArray_getData(state_array_in);
+	uint16_t *state_out = naFuns->MNumericArray_getData(state_array_out);
 
-	wireworld_step_impl(state_in, state_out, dims[0], dims[1]);
+	wireworld_step_immutable_impl(state_in, state_out, dims[0], dims[1]);
 
-	MArgument_setMTensor(res, state_tensor_out);
+	MArgument_setMNumericArray(res, state_array_out);
+	return error;
+}
+
+EXTERN_C DLLEXPORT int wireworld_step_mutable(WolframLibraryData libData, mint argc, MArgument *args, MArgument res)
+{
+	mint error = LIBRARY_NO_ERROR;
+
+	if (argc != 1) {
+		return LIBRARY_FUNCTION_ERROR;
+	}
+
+	WolframNumericArrayLibrary_Functions naFuns = libData->numericarrayLibraryFunctions;
+	MNumericArray state_array = MArgument_getMNumericArray(args[0]);
+	if (naFuns->MNumericArray_getRank(state_array) != 2) {
+		return LIBRARY_RANK_ERROR;
+	}
+	if (naFuns->MNumericArray_getType(state_array) != MNumericArray_Type_UBit16) {
+		return LIBRARY_TYPE_ERROR;
+	}
+
+	const mint *dims = naFuns->MNumericArray_getDimensions(state_array);
+	uint16_t *state = naFuns->MNumericArray_getData(state_array);
+
+	wireworld_step_mutable_impl(state, dims[0], dims[1]);
+
 	return error;
 }
--- libWireworld/wireworld.c
+++ libWireworld/wireworld.c
@@ -19,9 +19,9 @@ enum WIREWORLD_CELL {
 
 	Electron heads and photon heads are considered to be alive.
 */
-static inline int is_alive(int cell)
+static inline int is_alive(uint8_t cell)
 {
-	return (ELECTRON_HEAD == cell || PHOTON_HEAD == cell) ? 1 : 0;
+	return (ELECTRON_HEAD == cell || PHOTON_HEAD == cell);
 }
 
 static inline int cyclic_pos(int pos, int size)
@@ -29,7 +29,7 @@ static inline int cyclic_pos(int pos, int size)
 	return (pos + size) % size;
 }
 
-static inline int count_live_neighbors(int64_t *state, int rows, int cols, int row, int col)
+static inline int count_live_neighbors(uint16_t *state, int rows, int cols, int row, int col)
 {
 	int count = 0;
 	int pad;
@@ -64,66 +64,96 @@ static inline int count_live_neighbors(int64_t *state, int rows, int cols, int r
 	return count;
 }
 
-static void wireworld_step_impl(int64_t *state_in, int64_t *state_out, int rows, int cols)
+static inline uint8_t evolve_cell(uint16_t* state, int rows, int cols, int row, int col, uint8_t cell_in)
 {
-	int64_t *cell_in = state_in;
-	int64_t *cell_out = state_out;
+	uint8_t cell_out;
+	switch (cell_in) {
+		/*
+			1. Wire cells with 1 or 2 live neighbors (electron heads or
+			photon heads) turn into electron heads.
+		*/
+		case WIRE: {
+			int count = count_live_neighbors(state, rows, cols, row, col);
+			cell_out = (1 == count || 2 == count) ? ELECTRON_HEAD : WIRE;
+		} break;
+		/*
+			2. Electron heads turn into electron tails.
+		*/
+		case ELECTRON_HEAD: {
+			cell_out = ELECTRON_TAIL;
+		} break;
+		/*
+			3. Electron tails turn into wire cells.
+		*/
+		case ELECTRON_TAIL: {
+			cell_out = WIRE;
+		} break;
+		/*
+			4. Vacuum cells with 2 or 3 live neighbors (electron heads or
+			photon heads) turn into photon heads.
+		*/
+		case VACUUM: {
+			int count = count_live_neighbors(state, rows, cols, row, col);
+			cell_out = (2 == count || 3 == count) ? PHOTON_HEAD : VACUUM;
+		} break;
+		/*
+			5. Photon heads turn into photon tails.
+		*/
+		case PHOTON_HEAD: {
+			cell_out = PHOTON_TAIL;
+		} break;
+		/*
+			6. Photon tails turn into vacuum cells.
+		*/
+		case PHOTON_TAIL: {
+			cell_out = VACUUM;
+		} break;
+
+		/* Empty cells do not evolve. */
+		case EMPTY: {
+			cell_out = EMPTY;
+		} break;
+
+		/* Unknown cells become empty cells. */
+		default: {
+			cell_out = EMPTY;
+		} break;
+	}
+	return cell_out;
+}
+
+static void wireworld_step_immutable_impl(uint16_t *state_in, uint16_t *state_out, int rows, int cols)
+{
+	uint16_t *cell_in = state_in;
+	uint16_t *cell_out = state_out;
 	for (int row = 0; row < rows; row += 1) {
 		for (int col = 0; col < cols; col += 1) {
-			switch (*cell_in) {
-				/*
-					1. Wire cells with 1 or 2 live neighbors (electron heads or
-					photon heads) turn into electron heads.
-				*/
-				case WIRE: {
-					int count = count_live_neighbors(state_in, rows, cols, row, col);
-					*cell_out = (1 == count || 2 == count) ? ELECTRON_HEAD : WIRE;
-				} break;
-				/*
-					2. Electron heads turn into electron tails.
-				*/
-				case ELECTRON_HEAD: {
-					*cell_out = ELECTRON_TAIL;
-				} break;
-				/*
-					3. Electron tails turn into wire cells.
-				*/
-				case ELECTRON_TAIL: {
-					*cell_out = WIRE;
-				} break;
-				/*
-					4. Vacuum cells with 2 or 3 live neighbors (electron heads or
-					photon heads) turn into photon heads.
-				*/
-				case VACUUM: {
-					int count = count_live_neighbors(state_in, rows, cols, row, col);
-					*cell_out = (2 == count || 3 == count) ? PHOTON_HEAD : VACUUM;
-				} break;
-				/*
-					5. Photon heads turn into photon tails.
-				*/
-				case PHOTON_HEAD: {
-					*cell_out = PHOTON_TAIL;
-				} break;
-				/*
-					6. Photon tails turn into vacuum cells.
-				*/
-				case PHOTON_TAIL: {
-					*cell_out = VACUUM;
-				} break;
-
-				/* Empty cells do not evolve. */
-				case EMPTY: {
-					*cell_out = EMPTY;
-				} break;
-
-				/* Unknown cells become empty cells. */
-				default: {
-					*cell_out = EMPTY;
-				} break;
-			}
+			*cell_out = evolve_cell(state_in, rows, cols, row, col, *cell_in);
 			cell_in += 1;
 			cell_out += 1;
 		}
+	}
+}
+
+static void wireworld_step_mutable_impl(uint16_t *state, int rows, int cols)
+{
+	uint16_t *raw_cell;
+
+	raw_cell = state;
+	for (int row = 0; row < rows; row += 1) {
+		for (int col = 0; col < cols; col += 1) {
+			/* Store the old value in the low 16-bits of the cell. */
+			int cell_out = evolve_cell(state, rows, cols, row, col, *raw_cell);
+			/* Store the new value in the high 16-bits of the cell. */
+			*raw_cell |= cell_out << 8;
+			raw_cell += 1;
+		}
+	}
+
+	raw_cell = state;
+	/* Move the high 16-bits to the low 16-bits. */
+	for (int i = 0; i < rows * cols; i += 1) {
+		*raw_cell >>= 8;
+		raw_cell += 1;
 	}
 }
